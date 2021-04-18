19e07ea07472c2fe6d49787e1eaf1acb15c96529
Dhyey Moliya <dhyeymoliya@gmail.com>
2021-04-18 18:21:38 +0530

    Init

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,8 @@
+<h1>Website Layout</h1>
+<slot />
+
+<style>
+	:global(h2) {
+		color: orange;
+	}
+</style>
---
+++ src/routes/admin/$layout.reset.svelte
@@ -0,0 +1,8 @@
+<h1>Admin Layout</h1>
+<slot />
+
+<style>
+	:global(h2) {
+		color: red;
+	}
+</style>
